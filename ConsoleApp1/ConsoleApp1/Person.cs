544d33d9e3b1417ebedb8c640053d3cf7e453f7c
RuncanMarian <runcan_marian@yahoo.com>
2018-12-22 10:25:57 +0200

    Extention method

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public DateTime BirthDate { get => DateTime.Now; } //metoda Lambda mai simpla
+
+        public DateTime BirthDate2
+        {
+            get { return DateTime.Now; }
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; //must add if we use lambda expresion
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "My awesome students";
+            int wNo = Extension.WordCount(myString);
+            int wNoFromExtension = myString.WordCountExtension();
+
+            var now = DateTime.Now;
+
+            Console.WriteLine(now.RoundToHalfHour());
+
+
+            List<Person> persons = new List<Person>();
+
+            persons.Add(new Person()
+
+            {
+                City = "Suceava",
+                FirstName = "Marian",
+                LastName = "Runcan",
+                Country = "Romania"
+
+
+
+            });
+
+            persons.Add(new Person()
+
+            {
+                City = "Bihor",
+                FirstName = "Andrei",
+                LastName = "Mairean",
+                Country = "Bulgaria"
+
+
+
+            });
+
+            persons.Add(new Person()
+
+            {
+                City = "Cluj",
+                FirstName = "Ionut",
+                LastName = "Andrei",
+                Country = "Belgia"
+
+
+
+            });
+
+            var student = new //Anonymous types
+            {
+                Name = "student 1",
+                Course = ".Net"
+            };
+            Console.WriteLine(student.Course + student.Name);
+
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            int oddNumbers = numbers.Count(n => n % 2 == 1); //Must add LINQ(language integrated query)
+
+
+            var personInity = persons.Where(person => person.City == "Iasi")
+                .OrderBy(x=>x.FirstName)
+                .OrderByDescending(x=>x.Country.WordCountExtension()).ToList();
+
+            var personInCityClass = new List<Person>();
+            foreach (var person in persons)
+            {
+                if(person.City == "Iasi")
+                {
+                    personInCityClass.Add(person);
+                }
+
+            }
+
+             
+
+        } 
+
+    }
+}
